28569628cd29786477caeca2c2b3fedb5c61a9b0
Fabio Pavesi <fabio@adamassoft.it>
2022-03-27 17:53:39 +0200

    Trying to push

--- dockerise.sh
+++ dockerise.sh
@@ -6,6 +6,6 @@ git commit -m "$message" && \
 IMAGE_NAME=fabiopav/edi-ng_client && \
 npm version patch && \
 VERSION=$(cat package.json | jq -r ".version") && \
-docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest && \
+docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest . && \
 docker push $IMAGE_NAME:$VERSION
 docker push $IMAGE_NAME:latest
